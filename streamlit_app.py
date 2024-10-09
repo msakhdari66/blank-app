1a930a0910cc4775a48c4ce1fdbc8f94897644ec
msakhdari66 <msakhdari@blueorigin.com>
2024-10-09 11:49:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.regression.linear_model import OLS
 from statsmodels.tools.tools import add_constant
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-from scipy import stats
-from scipy.interpolate import interp1d
-from scipy.stats import binom
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.preprocessing import StandardScaler
+# from scipy import stats
+# from scipy.interpolate import interp1d
+# from scipy.stats import binom
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import classification_report
 #from io import BytesIO
 #
 st.write('hi hi')
