58e70b5673af63c3720340d35d831d443f3d1bc7
msakhdari66 <msakhdari@blueorigin.com>
2024-10-09 11:35:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-pip install matplotlib
 import matplotlib.pyplot as plt
 # from statsmodels.regression.linear_model import OLS
 # from statsmodels.tools.tools import add_constant
